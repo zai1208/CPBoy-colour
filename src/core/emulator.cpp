1874208ac5e2448fe4f0b7a774991c0d735010ea
zai1208 <zai1208@outlook.com>
2024-09-03 09:57:25 +1000

    Update emulator.cpp

--- src/core/emulator.cpp
+++ src/core/emulator.cpp
@@ -460,6 +460,8 @@ uint8_t run_emulator(struct gb_s *gb, emu_preferences *prefs)
 
 uint8_t load_rom(emu_preferences *prefs)
 {
+	Debug_SetCursorPosition(0, 0);
+	Debug_PrintString("REACHED BEFORE ERROR", false);
   char rom_filename[MAX_FILENAME_LEN] = DIRECTORY_ROM "\\";
   strncat(rom_filename, prefs->current_filename, MAX_FILENAME_LEN - 1);
   rom_filename[MAX_FILENAME_LEN - 1] = '\0';
