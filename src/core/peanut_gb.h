0651fc14bcba897247744b41597d6394b8eb62b6
zai1208 <zai1208@outlook.com>
2024-10-08 09:14:35 +1100

    Update peanut_gb.h

--- src/core/peanut_gb.h
+++ src/core/peanut_gb.h
@@ -116,7 +116,7 @@
 /* Memory section sizes for DMG */
 #if PEANUT_FULL_GBC_SUPPORT
 #define WRAM_SIZE	0x8000
-#define VRAM_SIZE	0x2000
+#define VRAM_SIZE	0x4000
 #else
 #define WRAM_SIZE	0x2000
 #define VRAM_SIZE	0x2000
